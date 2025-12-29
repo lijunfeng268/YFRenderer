19aa0ae6b128ecaaa4be67f0700b53a2bd455fda
junfeng.li <junfeng.li@mthreads.com>
2025-12-29 18:17:09 +0800

    add draw

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include "Framebuffer.h"
 #include <iostream>
 #include <cmath>
+#include<string>
 
 // 绘制一些测试像素
 Color white(255, 255, 255, 255);
 Color red(255, 0, 0, 255);
 Color green(0, 255, 0, 255);
 Color blue(0, 0, 255, 255);
+Color yellow  = {  0, 200, 255, 255};
 
 void line(int ax, int ay, int bx, int by, Framebuffer& framebuffer, Color color = white) {
     for (float t=0.; t<1.; t+=.02) {
@@ -40,22 +42,9 @@ Framebuffer test(int width, int height)
     return framebuffer;
 }
 
-int main(int argc, char** argv) {
-    
-    constexpr int width  = 64;
-    constexpr int height = 64;
-    Window window("Software Renderer", width, height);
-    //Framebuffer framebuffer = test(width, height);
-
-    Framebuffer framebuffer(width, height);
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
-    line(ax, ay, bx, by, framebuffer, red);
-
-    std::cout << "Window created: " << width << "x" << height << std::endl;
-    std::cout << "Press ESC or close window to exit" << std::endl;
-
+void draw(std::string windowName,Framebuffer& framebuffer,int width, int height)
+{
+    Window window(windowName.c_str(), width, height);
     // 主循环
     while (!window.shouldClose()) {
         // 处理窗口事件
@@ -67,6 +56,29 @@ int main(int argc, char** argv) {
         // 简单的延时，避免CPU占用过高
         Sleep(16);  // 约60 FPS
     }
+}
+
+int main(int argc, char** argv) {
+    
+    constexpr int width  = 640;
+    constexpr int height = 480;
+    std::string windowName = "Software Renderer";
+    //Window window("Software Renderer", width, height);
+    //Framebuffer framebuffer = test(width, height);
+
+    Framebuffer framebuffer(width, height);
+    int ax =  7, ay =  3;
+    int bx = 12, by = 37;
+    int cx = 62, cy = 53;
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
+
+    std::cout << "Window created: " << width << "x" << height << std::endl;
+    std::cout << "Press ESC or close window to exit" << std::endl;
+
+    draw(windowName,framebuffer,width,height);
 
     std::cout << "Window closed" << std::endl;
     return 0;
